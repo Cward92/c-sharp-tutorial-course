408c160a4bf26d58aabc8d64d9d1f16d62cc9350
chandlermward92 <chandlermward92@gmail.com>
2021-02-11 19:37:05 -0500

    add basic rules and while loops for adding guess restrictions

--- Program.cs
+++ Program.cs
@@ -8,15 +8,31 @@ namespace workspace
         {
            string guessWord = "elephant";
            string guess = "";
+           int guessCount = 0;
+           int guessLimit = 5;
+           bool outOfGuesses = false;
            
-             while(guess != guessWord)
+             while(guess != guessWord && !outOfGuesses)
              {
-               Console.Write("Enter guess: ");
-               guess = Console.ReadLine();
+               if(guessCount < guessLimit)
+               {
+                 Console.Write("Enter guess: ");
+                 guess = Console.ReadLine();
+                 guessCount++;
+               }
+               else 
+               {
+                 outOfGuesses = true;  
+               }
              }
-            
-            Console.Write("You win!");
-          
+            if(outOfGuesses)
+            {
+              Console.Write("You lose!");
+            }
+            else 
+            {
+              Console.Write("You win!");
+            }
             Console.ReadLine();
         }
     }
